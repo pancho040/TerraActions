2bb6cd75f9cfaa58b9c2cd8c5d2d7bbde24a4501
panchito04 <molinafranz05@gmail.com>
2025-10-28 17:27:50 -0400

    pro

--- infra/main.tf
+++ infra/main.tf
@@ -18,7 +18,14 @@ resource "oci_core_instance" "ubuntu_vm" {
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
-    user_data           = base64encode(templatefile("${path.module}/cloud_init.sh", {}))
+
+    # ⚠️ Aquí se pasan las variables al template del cloud-init
+    user_data = base64encode(templatefile("${path.module}/cloud_init.sh", {
+      DOCKER_USER   = var.docker_user
+      SUPA_BASE_URL = var.supa_base_url
+      SUPA_ANON_KEY = var.supa_anon_key
+      JWT_SECRET    = var.jwt_secret
+    }))
   }
 
   freeform_tags = {
@@ -26,10 +33,16 @@ resource "oci_core_instance" "ubuntu_vm" {
   }
 }
 
+# -----------------------
+#  SALIDAS (outputs)
+# -----------------------
+
 output "public_ip" {
-  value = oci_core_instance.ubuntu_vm.public_ip
+  description = "IP pública de la instancia desplegada"
+  value       = oci_core_instance.ubuntu_vm.public_ip
 }
 
 output "instance_id" {
-  value = oci_core_instance.ubuntu_vm.id
+  description = "OCID de la instancia desplegada"
+  value       = oci_core_instance.ubuntu_vm.id
 }
